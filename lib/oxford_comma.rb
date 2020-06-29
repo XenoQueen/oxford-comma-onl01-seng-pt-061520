3cb372c15f827fe4b053c18d5da3a68a75010069
Maika Kailani <queenmab051@gmail.com>
2020-06-29 18:37:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     array[0]
   elsif array.length == 2
-    return array.join(" and ")
+    array.join(" and ")
   elsif array.length == 3
       array[0..-2].join(", ") + ", and " + array.last
   else array.length > 3
